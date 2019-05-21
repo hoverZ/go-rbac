b8405be28d2b038fb3004e8d5c6197f0260368cb
chenyuzhe <chenyuzhe@forchange.tech>
2019-05-21 10:50:14 +0800

    feat(code) tree&test
    关联issue：https://github.com/hoverZ/go-rbac/issues/5

--- permission/tree_test.go
+++ permission/tree_test.go
@@ -5,11 +5,19 @@ import (
 	"testing"
 )
 
-func TestFindLeafNode(t *testing.T) {
-
-}
-
+// InsertTreeNode 测试用例：
+// 1. 插入后的树结构
+// 2. 插入根节点 path=""
+// 3. 插入多级节点 path="/a", path="/a/b"
+// 4. 插入模糊节点 path="/:t", path="/:m/abc"
+// 5. 插入模糊节点 path="/:t", path="/a"
+// FindLeafNode 测试用例：
+// 1. 初始化节点
+// 根节点
+// 2. 授权时查询
+// 域名
 func TestInsertTreeNode(t *testing.T) {
+	// InsertTreeNode 测试用例：
 	insertNodeItems := []*InsertNodeItem{
 		&InsertNodeItem{
 			Id:     1,
@@ -77,4 +85,5 @@ func TestInsertTreeNode(t *testing.T) {
 		So(len(tree.RootNode.Children[2].Children), ShouldEqual, 1)
 		So(tree.RootNode.Children[2].Children[0].Id, ShouldEqual, 6)
 	})
+	// FindLeafNode 测试用例：
 }
